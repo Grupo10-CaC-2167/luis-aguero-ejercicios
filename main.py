617886efdaba7b6050f0f0f92305a43e3af5b324
luisaguero92 <88601920+luisaguero92@users.noreply.github.com>
2021-11-16 14:56:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from src.utils import replacespace
+from src.utils import *
 
 #Ejercicio 1
-palabra = "hola como estas"
-replacespace(palabra)
+palabra1 = "hola como estas"
+replacespace(palabra1)
+print(palabra1)
 print (replacespace(palabra))
+
+#Ejercicio 2
+palabra2 = "hOLA mUNDO"
+changeMayus(palabra2)
+print(palabra2)
+print(changeMayus(palabra2))
+
+#Ejercicio 3
+
+
+#Ejercicio 4
+
+
+#Ejercicio 5
+
