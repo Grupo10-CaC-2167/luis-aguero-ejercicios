bb123ef6cfbd6f879d2084fd9434f7924f942c58
luisaguero92 <luisaguero92@gmail.com>
2021-11-15 22:25:47 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.utils import replacespace
+
+#Ejercicio 1
+palabra = "hola como estas"
+replacespace(palabra)
+print (replacespace(palabra))
---
+++ src/utils.py
@@ -0,0 +1,4 @@
+#Ejercicio 1
+def replacespace (string):
+
+      return string.replace(" ","_")
